7e4d9a62c5ad811aed22551f7dfe8627d573fe2a
Vasya Pupkin <vasya.pupkin@email.com>
2023-09-06 20:37:53 +0300

    1 задача

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.WriteLine ("Введите число N:");
+int Z =int.Parse (Console.ReadLine ()!);
+
+Console.WriteLine ($"{Natur(Z,1)}");
+
+string Natur(int N,int K)
+{
+    if(N==K) return K.ToString();
+    return (N+","+ Natur(N-1,K));
+   
+}
