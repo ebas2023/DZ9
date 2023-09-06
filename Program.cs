9d9b439952f9fe582dd68e8671d1e2f3bd2ed7f3
Vasya Pupkin <vasya.pupkin@email.com>
2023-09-06 21:13:49 +0300

    3 задача

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
-﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-M = 1; N = 15 -> 120
-M = 4; N = 8. -> 30*/
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
 
-Console.WriteLine ("Введите число N:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Введите число M:");
+Console.WriteLine("Введите число N:");
 int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M:");
+int m = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine ($"Сумма равна={Natur(m,n)}");
+Console.WriteLine($"Сумма равна={A(n, m)}");
 
-int Natur(int m,int n)
+int A(int n, int m)
 {
-    if(m==n) return m;
-    return Natur(m+1,n)+m; 
+    if (n == 0) return m + 1;
+    if ((n != 0) && (m == 0))
+        return A(n - 1, 1);
+    else
+        return A(n - 1, A(n, m - 1));
 }
 
 
